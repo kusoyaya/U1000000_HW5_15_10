aadb95d7af4213e6ac04fd27d1e95200637b6a8f
NasirHo <nasirho@NasirHos-MacBook-Air.local>
2015-04-17 16:59:18 +0800

    重新撰寫 testprogram 以說明
    撰寫 test program 以及完成註解

--- src/hw1510/MyStack.java
+++ src/hw1510/MyStack.java
@@ -4,33 +4,37 @@ import java.util.ArrayList;
 import java.io.*;
 
 public class MyStack<E> implements Cloneable,Serializable{
-	private ArrayList<E> list = new ArrayList<E>();
+	private ArrayList<E> list = new ArrayList<E>(); //create Arraylist
 	
 	public boolean isEmpty(){
 		return list.isEmpty();
-	}
+	}//stack isempty is same as arraylist isempty
 	
 	public int getSize(){
 		return list.size();
-	}
+	}//stack getsize is same as arraylist size
 	
 	public E peek(){
 		return list.get(getSize()-1);
-	}
+	}//stack peek is to take last element in arraylist
 	
 	public E pop(){
 		E e = list.get(getSize()-1);
 		list.remove(getSize()-1);
 		return e;
-	}
+	}//stack pop is to take last element in arraylist and then remove it
 	
 	public void push(E e){
 		list.add(e);
-	}
+	}//stack push is same as arraylist add
 	
 	public E get(int i){
 		return list.get(i);
-	}
+	}//this is for the test program to get the index element in arraylist
+	
+	public ArrayList<E> getList(){
+		return list;
+	}//this is for the test program to get the arraylist
 	
 	public Object clone(){
 		try{
@@ -38,7 +42,7 @@ public class MyStack<E> implements Cloneable,Serializable{
 		}catch(CloneNotSupportedException ex){
 			return null;
 		}
-	}
+	}//clone stack itself but not clone the list
 	
 	public Object shallowClone(){
 		try{
@@ -48,25 +52,25 @@ public class MyStack<E> implements Cloneable,Serializable{
 		}catch(CloneNotSupportedException ex){
 			return null;
 		}
-	}
+	}//clone the stack and list but not clone the object in arraylist
 	
 	public MyStack<E> deepClone(){
 		try{
-			ByteArrayOutputStream baos = new ByteArrayOutputStream();
-			ObjectOutputStream oos = new ObjectOutputStream(baos);
-			oos.writeObject(this);
+			ByteArrayOutputStream baos = new ByteArrayOutputStream(); //create a bytearray outputstream
+			ObjectOutputStream oos = new ObjectOutputStream(baos); //let the object stream to the bytearray outputstream above
+			oos.writeObject(this); //write this object into stream
 			
-			ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
-			ObjectInputStream ois = new ObjectInputStream(bais);
-			return (MyStack<E>) ois.readObject();
+			ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray()); //create a bytearray inputstream
+			ObjectInputStream ois = new ObjectInputStream(bais);  //let the object stream to the bytearray inputstream above
+			return (MyStack<E>) ois.readObject(); //read the object from stream and return it
 		}catch(Exception e){
 			System.out.println("Object in stack cannot be seriealized.\nDo shallowclone.");
 			return (MyStack<E>) this.shallowClone();
 		}
-	}
+	}//using serialize to clone all thing in stack
 	
 	@Override
 	public String toString(){
 		return "Stack"+list.toString();
-	}
+	}//override the tostring method
 }
--- src/hw1510/MyStackCloneTestDrive.java
+++ src/hw1510/MyStackCloneTestDrive.java
@@ -4,37 +4,73 @@ import java.io.Serializable;
 
 public class MyStackCloneTestDrive {
 	public static void main(String[] args){
-		MyStack<Person> stack1 = new MyStack<Person>();
-		stack1.push(new Person("A"));
-		stack1.push(new Person("B"));
-		stack1.push(new Person("C"));
-		MyStack<Person> stack2 = (MyStack<Person>) (stack1.clone());
-		MyStack<Person> stack3 = (MyStack<Person>) (stack1.shallowClone());
-		MyStack<Person> stack4 = stack1.deepClone();
-		stack1.get(2).setName("CC");
-		stack1.push(new Person("D"));
-		stack1.push(new Person("E"));
-		stack1.push(new Person("F"));
-		System.out.println(stack1.getSize());
-		System.out.println(stack2.getSize());
-		System.out.println(stack3.getSize());
-		System.out.println(stack4.getSize());
+		System.out.println("本程式將說明 Shallow Copy 以及 Deep Copy 的差別。");
 		
-		System.out.println(stack2.get(2).name);
-		System.out.println(stack3.get(2).name);
-		System.out.println(stack4.get(2).name);
+		MyStack<Person> originalStack = new MyStack<Person>();
+		originalStack.push(new Person("John"));
+		originalStack.push(new Person("Peter"));
+		originalStack.push(new Person("Tom"));
+		System.out.println("先將三個不同名字的 Person 物件推入 original stack 當中。\n");
+		
+		MyStack<Person> stack1 = (MyStack<Person>) originalStack.clone();
+		MyStack<Person> stack2 = (MyStack<Person>) originalStack.shallowClone();
+		MyStack<Person> stack3 = originalStack.deepClone();
+		System.out.println("建立三個 stack 分別是:\nstack1:只複製 stack 本身，list 沒有複製到\nstack2:複製 stack 以及 list\nstack3:連同 list 裡面的物件也複製\n");
+		
+		originalStack.push(new Person("Jane"));
+		originalStack.push(new Person("Mary"));
+		originalStack.push(new Person("Amy"));
+		System.out.println("再推入三個不同名字的 Person 物件到 original stack 當中。");
+		System.out.printf("目前 original stack 裡面的 list 共有 %d 個Person。\nstack1 裡面的 list 有 %d 個 Person。\nstack2 裡面的 list 有 %d 個 Person\nstack3 裡面的list 有 %d 個 Person。\n",originalStack.getSize(),stack1.getSize(),stack2.getSize(),stack3.getSize());
+		System.out.println("由此可知 stack1 裡的 list 仍然和 original stack 裡面的相同，並沒有 deep copy 到。\n");
+		
+		originalStack.get(0).setName("Hilson");
+		System.out.println("再把 John 的名字改成 Hilson。");
+		System.out.println("目前 original stack 裡頭第一個 Person 的名字是"+originalStack.get(0).getName());
+		System.out.println("stack1 裡頭第一個 Person 的名字是 "+stack1.get(0).getName());
+		System.out.println("stack2 裡頭第一個 Person 的名字是 "+stack2.get(0).getName());
+		System.out.println("stack3 裡頭第一個 Person 的名字是 "+stack3.get(0).getName());
+		System.out.println("由此可知 stack1 和 stack2 裡頭第一個 Person 仍然和 original stack 的相同，並沒有 deep copy 到。\n");
+		
+		System.out.println("以下將利用 toString 方法列出各個 stack 裡面元素以便確認。");
+		System.out.println("original stack 是: "+originalStack.toString());
+		System.out.println("stack1 是: "+stack1.toString());
+		System.out.println("stack2 是: "+stack2.toString());
+		System.out.println("stack3 是: "+stack3.toString());
+		
+		System.out.println("\n以下是利用 == operator 來確認是否 deep copy");
+		if(originalStack.getList()==stack1.getList()){
+			System.out.println("stack1 裡面的 list 和 orignal stack 相同");
+		}else{
+			System.out.println("stack1 裡面的 list 和 orignal stack 不同");
+		}
+		if(originalStack.getList()==stack2.getList()){
+			System.out.println("stack2 裡面的 list 和 orignal stack 相同");
+		}else{
+			System.out.println("stack2 裡面的 list 和 orignal stack 不同");
+		}
+		if(originalStack.getList()==stack3.getList()){
+			System.out.println("stack3 裡面的 list 和 orignal stack 相同");
+		}else{
+			System.out.println("stack3 裡面的 list 和 orignal stack 不同");
+		}
+		
+		if(originalStack.get(1)==stack1.get(1)){
+			System.out.println("stack1 裡面的 Person 和 orignal stack 相同");
+		}else{
+			System.out.println("stack1 裡面的 Person 和 orignal stack 不同");
+		}
+		if(originalStack.get(1)==stack2.get(1)){
+			System.out.println("stack2 裡面的 Person 和 orignal stack 相同");
+		}else{
+			System.out.println("stack2 裡面的 Person 和 orignal stack 不同");
+		}
+		if(originalStack.get(1)==stack3.get(1)){
+			System.out.println("stack3 裡面的 Person 和 orignal stack 相同");
+		}else{
+			System.out.println("stack3 裡面的 Person 和 orignal stack 不同");
+		}
 	}
 }
 
 
-class Person implements Serializable{
-	String name;
-	
-	Person(String name){
-		this.name = name;
-	}
-	
-	public void setName(String name){
-		this.name = name;
-	}
-}
